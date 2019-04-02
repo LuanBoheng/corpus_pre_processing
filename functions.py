0681448417b13945505bd62a3b7e5bae7b3715ec
LuanBoheng <314887027@qq.com>
2019-04-02 17:13:28 +0800

    review

--- functions.py
+++ functions.py
@@ -4,10 +4,10 @@ from functools import partial, reduce
 import multiprocessing
 from tqdm import tqdm
 
-corpus_list = []
+corpus_list = []  # type: List[str]
 
 
-def load_corpus_list(path):
+def load_corpus_list(path: str):
     global corpus_list
     with open(path, 'r') as f:
         corpus_list = f.read().split('\n')
@@ -20,24 +20,24 @@ def get_ngram(sents: list, n: int) -> Counter:
     return Counter(ngram)
 
 
-def multi_processing(batch_file: list, target_func) -> list:
+def multi_processing(batch_file: list, mapper: callable, reducer: callable) -> list:
     pool = multiprocessing.Pool()
-    ngram_counts = pool.map(target_func, batch_file)
+    ngram_counts = pool.map(mapper, batch_file)
     pool.close()
     pool.join()
-    return reduce(lambda x, y: x + y, ngram_counts)
+    return reduce(reducer, ngram_counts)
 
 
-def cut_list(data, batch_size) -> list:
+def list_split(data: list, batch_size: int) -> List[list]:
     return [data[x:x+batch_size] for x in range(0, len(data), batch_size)]
 
 
-def process_batch(input_list: list, batch_size: int = 252) -> Counter:
+def get_ngram_batch(input_list: list, batch_size: int = 252) -> Counter:
     ngram_freq = Counter()
-    corpus_batch = cut_list(input_list, batch_size)
+    corpus_batch = list_split(input_list, batch_size)
 
     for batch in tqdm(corpus_batch):
-        sents = multi_processing(batch, file2sent)
+        sents = multi_processing(batch, file2sent, lambda x,y: x+y)
         for n in [1, 2, 3]:
             ngram_freq += Counter({k: v for k, v in get_ngram(sents, n).items() if v > 1})
 
@@ -60,7 +60,7 @@ def load_obj(file_name):
 if __name__ == '__main__':
     corpus_name = 'input_corpus.txt'
     load_corpus_list(corpus_name)
-    ngram_freq = process_batch(corpus_list)
+    ngram_freq = get_ngram_batch(corpus_list)
     save_obj(ngram_freq, 'output/wiki_gram_123.model')
 
 
--- pre_processing_wiki.py
+++ pre_processing_wiki.py
@@ -8,11 +8,11 @@ zh_pattern = re.compile('[\u4e00-\u9fa5]')  # chinese word pattern
 def chinese_in_string(input_string):
     return zh_pattern.search(input_string)
 
-
 def file2sent(file_path:str):            # return a list(file) of list(sent)
     text = str(open(file_path, 'r').read())
     text = re.sub("<.+?>", '', text)        # delete html tags
-    text = re.sub("\d+", '0', text)         # convert all numbers to 0
+    text = re.sub("\(.+?\)", '', text)  # delete html tags
+    # text = re.sub("\d{4}\d+", '9999', text)         # convert all numbers > 9999 to 9999
     sents = re.findall("[\w,\d]+", text)    # break text to sentences
     sents = [s for s in sents if zh_pattern.search(s)]  # only consider sentence who have chinese word
     sents = [jieba.lcut(s) for s in sents]  # cut sent via jieba
@@ -23,4 +23,6 @@ def file2sent(file_path:str):            # return a list(file) of list(sent)
 
 if __name__ == '__main__':
     sents = file2sent('/nlp_project/big_files/wiki_corpus/wiki_chs/AG/wiki_42')
-    print(sents)
+    # for s in sents:
+    #     print(''.join(s))
+    print('avg len of', len(sents), 'sents:', sum(len(s) for s in sents) / len(sents))
