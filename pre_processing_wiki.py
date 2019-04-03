b2ce386811de0c16dfb681d5d1439f4b0e0a6ef4
LuanBoheng <314887027@qq.com>
2019-04-03 18:37:01 +0800

    file to table ongoning

--- pre_processing_wiki.py
+++ pre_processing_wiki.py
@@ -8,7 +8,15 @@ zh_pattern = re.compile('[\u4e00-\u9fa5]')  # chinese word pattern
 def chinese_in_string(input_string):
     return zh_pattern.search(input_string)
 
-def file to
+def file_to_table(file_path: str):
+    file = str(open(file_path, 'r').read())
+    docs = file.split('</doc>')
+    for doc in docs:
+        info = re.findall(' (.+?)=\"(.+?)\"', doc)
+        text = re.sub("<.+?>", '', doc)
+
+
+
 # def file2sent(file_path:str):            # return a list(file) of list(sent)
 #     text = str(open(file_path, 'r').read())
 #     text = re.sub("<.+?>", '', text)        # delete html tags
@@ -23,7 +31,7 @@ def file to
 
 
 if __name__ == '__main__':
-    sents = file2sent('/nlp_project/big_files/wiki_corpus/wiki_chs/AG/wiki_42')
+    sents = file_to_table('/nlp_project/big_files/wiki_corpus/wiki_chs/AG/wiki_42')
     # for s in sents:
     #     print(''.join(s))
-    print('avg len of', len(sents), 'sents:', sum(len(s) for s in sents) / len(sents))
+    # print(sents)
